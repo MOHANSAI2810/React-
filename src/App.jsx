4b06f1070cd2f9f22f00cc183eb73826a4c58f92
pandu <pushadapumohansai@gmail.com>
2025-05-14 22:04:22 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Apple from './Apple'
+import Sample from './components/Sample'
+import './sample.css'
+
+function App() {
+ return (
+  <div className='App'>
+    <Apple />
+    <Sample />
+  </div>
+ ) 
+}
+
+export default App
---
+++ src/components/Sample.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Test from './Test'
+const Mango=()=>{
+    return (
+        <h2>React Basics</h2>
+    )
+}
+
+function Sample() {
+  return (
+    <div>
+      <h3>Welcome</h3>
+      <Mango />
+      <Test/>
+    </div>
+  )
+}
+
+export default Sample
